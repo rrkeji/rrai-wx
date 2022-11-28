e7f4b531e02bb37c13a8ee814b70d0915ec9bf5c
rrkeji <rrkeji@163.com>
2022-11-28 16:33:00 +0800

    add

--- miniprogram/pages/chat/chat.ts
+++ miniprogram/pages/chat/chat.ts
@@ -1,4 +1,4 @@
-// bot.ts
+// chat.ts
 // 获取应用实例
 const io = require('../../utils/weapp.socket.io');
 // const socket = io('https://www.idns.link')
@@ -9,31 +9,6 @@ const socket = io('https://www.idns.link', {
   transports: ['websocket'], // 此项必须设置
 });
 
-// //是否与服务断开链接
-// //在连接成功（包括成功重新连接）时触发。
-// //socket.on（'connect'，（）=> { // ... }）;
-// socket.on('connect', () => {
-//   console.log(socket.disconnected); // false
-// });
-
-// //链接超时触发
-// socket.on('connect_timeout', (timeout) => {
-//   // ...
-// });
-
-// //重新尝试链接   错误时触发
-// socket.on('reconnect_error', (error) => {
-//   // ...
-// });
-
-// //无法在内部重新链接时触发
-// socket.on('reconnect_failed', () => {
-//   // ...
-// });
-
-
-// socket.close() //关闭链接
-
 Page({
 
   /**
@@ -42,6 +17,83 @@ Page({
   data: {
     newslist: <any>[],
     scrollTop: 0,
+    test: `<div class="rich_message" style="width:100%;">
+    <h2 style="color:red;">11月26日</h2>
+    <h2 style="color:red;border:2rpx solid #000000">十一月初三</h2>
+    <div class="row">
+      <div class="border col6">
+      <p class="title">
+        宜
+      </p>
+        <p>
+        乔迁 签合同 安灶 签约 交房 出差 进宅 领证 出门 搬家 迁居 出行 交易 安床 买房 买车 养殖 剃胎发 投资 入学 耕种 入宅 搬公司 安橱柜 
+        </p>
+      </div>
+      <div class="border  col6">
+      <p class="title">
+        忌
+      </p>
+        <p>
+        乔迁 签合同 安灶 签约 交房 出差 进宅 领证 出门 搬家 迁居 出行 交易 安床 买房 买车 养殖 剃胎发 投资 入学 耕种 入宅 搬公司 安橱柜 
+        </p>
+      </div>
+    </div>
+    <div  class="row">
+      <div class="border col6">
+      <p class="title">彭祖百忌</p>
+      <p>癸不词讼，理弱敌强  未不服药，毒气入肠 </p>
+      </div>
+      <div class="border col6">
+      <p class="title">凶煞宜忌</p>
+      <p>月火 月杀 勾陈黑道 五鬼 重丧 重复 月煞 月害 触水龙 土王用事 四击 复日 </p>
+      </div>
+    </div>
+    <div   class="row">
+      <div colspan="4" class="border col12">
+      <p class="title">吉神方位</p>
+      <p>喜神东南 财神正南 福神东北   阳贵东南  阴贵正东  </p>
+      </div>
+    </div>
+    <div   class="row">
+      <div class="border col6">
+        <p class="title">今日吉神</p>
+        <p>"月德", "四相", "大葬", "鸣吠", "不守塚", "母仓", "除神", </p>
+      </div>
+      <div class="border col6">
+        <p class="title">今日凶煞</p>
+        <p>"岁破", "天罡", "月害", "劫煞", "独火", "受死", "八座", "五离", "蚩尤", "伐日",</p>
+      </div>
+    </div>
+    <div  class="row">
+      <div class="border col3">
+        <p class="title">今日胎神</p>
+        <p>房床厕外西北</p>
+      </div>
+      <div class="border col3">
+        <p class="title">生肖冲煞</p>
+        <p>猴日冲虎</p>
+      </div>
+      <div class="border col3">
+      <p class="title">今日五行</p>
+      <p>"天干", "甲", "属木", "地支", "申"</p>
+      </div>
+    </div>
+    <div  class="row">
+      <div class="border col3">
+      <p class="title">八字</p>
+      <p>壬寅 辛亥 甲申 戊辰</p>
+      </div>
+      <div class="border col3">
+      <p class="title">星座</p>
+      <p>射手座</p>
+      </div>
+      <div class="border col3">
+      <p class="title">纳音</p>
+      <p>井泉水</p>
+      </div>
+    </div>
+  </div>
+  `,
     message: ""
   },
   /**
@@ -49,30 +101,69 @@ Page({
    */
   onLoad: function () {
     let that = this
-
+    socket.off("bot_uttered");
+    socket.off("connect");
+    wx.closeSocket();
     //是否链接到服务器
     socket.on('connect', () => {
       console.log(socket.connected); // true
     });
+    // //链接超时触发
+    // socket.on('connect_timeout', (timeout) => {
+    //   // ...
+    // });
+
+    // //重新尝试链接   错误时触发
+    // socket.on('reconnect_error', (error) => {
+    //   // ...
+    // });
+
+    // //无法在内部重新链接时触发
+    // socket.on('reconnect_failed', () => {
+    //   // ...
+    // });
+
     //为给定事件注册新的处理程序。
     //socket.on 接收的一些东西
     //news 可以作为后端通过 socket.emit 发的事件名 ，d 为发送的数据
     socket.on('bot_uttered', (d: { text: string }) => {
       console.log('received news: ', d)
-      var list: any[] = []
-      list = that.data.newslist
-      list.push({
+      let item: any = {
         "hidden": false,
         "nextMessageIsTooltip": false,
         "sender": "response",
         "showAvatar": false,
         "text": d.text,
+        "content": `<div class="rich_message"></div>`,
+        "url": "https://www.idns.link/#/./download",
+        "format": d.text,
+        "mediaId": d.text,
+        // 缩略图的媒体id
+        "thumbMediaId": d.text,
+        "locationX": d.text,
+        "locationY": d.text,
+        "scale": d.text,
+        "title": '标题',
+        "thumbPicUrl": 'https://img2.woyaogexing.com/2022/11/25/8374eb28f670c4c85c0d1104253a6b83.jpeg',
+        "description": d.text,
         "timestamp": 1667436405268,
-        "type": "text"
-      });
+        // text richtext image voice video shortvideo location link
+        "type": "richtext"
+      };
+      if (d.text && d.text.indexOf("{") == 0) {
+        let jsonItem = JSON.parse(d.text);
+        item = { ...item, ...jsonItem };
+      } else {
+        item['content'] = d.text;
+        item['type'] = 'text';
+      }
+      var list: any[] = []
+      list = that.data.newslist
+      list.push(item);
       that.setData({
         newslist: list
-      })
+      });
+      that.bottom();
     })
   },
   // 页面卸载
@@ -138,7 +229,6 @@ Page({
           }
         }
       });
-      this.bottom();
     }
   },
   //
@@ -179,4 +269,18 @@ Page({
       res[1] && res[1].scrollTop // 显示区域的竖直滚动位置  
     })
   },
+  //链接的跳转
+  linkGoTo: function (event: any) {
+    if (event.currentTarget && event.currentTarget.dataset &&
+      event.currentTarget.dataset.url) {
+      let url = event.currentTarget.dataset.url;
+      console.log(url);
+      wx.navigateTo({
+        url: `../webview/webview?url=${url}`,
+      })
+    }
+  },
 })
+
+
+
