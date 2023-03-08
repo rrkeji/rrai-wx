f9bb71ed43161d07382cdc57ca88c6463115fe37
i.xyz <i.xyz@foxmail.com>
2023-03-09 06:04:32 +0800

    add

--- miniprogram/pkg_mine/components/reward-logs/index.ts
+++ miniprogram/pkg_mine/components/reward-logs/index.ts
@@ -1,4 +1,6 @@
 // pkg_mine/components/reward-logs/index.ts
+import { searchRewardLogs } from '../../../services/index';
+
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +13,46 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    rewardLogs: <Array<any>>[],
+    page: 1,
+    pageSize: 30,
+    total: 0,
   },
+  lifetimes: {
+    attached: function () {
+      // 在组件实例进入页面节点树时执行
+      //reward logs
+      this.onRefresh();
+    },
+    detached: function () {
+      // 在组件实例被从页面节点树移除时执行
+    },
+  },
+  pageLifetimes: {
+    // 组件所在页面的生命周期函数
+    show: function () {
 
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    
+    onRefresh:function(){
+      searchRewardLogs(this.data.page, this.data.pageSize).then((res) => {
+        console.log(res);
+        this.setData({
+          total: res.total,
+          rewardLogs: res.data.map((item: any) => {
+            item.reason = item.reason.replace('${amount}', item.amount);
+            return item;
+          }),
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
   }
 })
+
